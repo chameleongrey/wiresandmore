15079f68521d905021325c77487102ca772dabde
s3805554 <s3805554@student.rmit.edu.au>
2024-03-22 19:43:41 +1100

    add event listeners to buttons

--- script/main.js
+++ script/main.js
@@ -1,5 +1,19 @@
-document.getElementById("domestic").addEventListener('click',function ()
-{
-    console.log("clicked");
-   }  ); 
+document.getElementById("domestic").addEventListener('click', function () {
+    console.log("Domestic");
+});
 
+document.getElementById("commercial").addEventListener('click', function () {
+    console.log("Commercial");
+});
+
+document.getElementById("testandtag").addEventListener('click', function () {
+    console.log("test & Tag");
+});
+
+document.getElementById("about").addEventListener('click', function () {
+    console.log("About Us");
+});
+
+document.getElementById("contact").addEventListener('click', function () {
+    console.log("Contact Us");
+});
