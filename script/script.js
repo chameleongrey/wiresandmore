5bb515f09183ca154d5b86197b4398756a3a4e78
David <pip_dev@outlook.com>
2024-07-17 09:46:44 +1000

    change of website structure

---
+++ script/script.js
@@ -0,0 +1,36 @@
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            const navHeight = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--nav-height')) * window.innerHeight / 100;
+            const headerHeight = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--header-height')) * window.innerHeight / 100;
+            let offsetTop = targetElement.offsetTop;
+
+            if (targetId === 'home') {
+                offsetTop -= (navHeight + headerHeight);
+                document.querySelectorAll('.grid-item.item-6').forEach(item => {
+                    item.style.visibility = 'hidden';
+                });
+            } else {
+                offsetTop -= .9 * window.innerHeight / 100;
+            }
+
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+
+            // Show grid-item item-6 after scrolling
+            if (targetId === 'home') {
+                setTimeout(() => {
+                    document.querySelectorAll('.grid-item.item-6').forEach(item => {
+                        item.style.visibility = 'visible';
+                    });
+                }, 500);
+            }
+        }
+    });
+});
